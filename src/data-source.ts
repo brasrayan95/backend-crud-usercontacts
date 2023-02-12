46f39a9c7eede850dcfb25f00ff159de71bcad94
Rayan Brasil <brasrayan@gmail.com>
2023-02-11 21:14:47 -0400

    fix: alteraçoes feitas para o deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,32 @@
-import { DataSource } from "typeorm";
+import "reflect-metadata"
+import path from "path"
+import { DataSource, DataSourceOptions } from "typeorm";
 require("dotenv").config()
 
-const AppDataSource = new DataSource(
-    process.env.NODE_ENV === "test"?
-    {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"]
-    } :
-    {
+
+const setDataSourceConfig = (): DataSourceOptions => {
+    const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
+    const migrationsPath: string = path.join(__dirname, "./migrations/**.{js,ts}");
+
+    if (process.env.NODE_ENV === "production") {
+        return {
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            entities: [entitiesPath],
+            migrations: [migrationsPath]
+        }
+    }
+
+    if (process.env.NODE_ENV === "test") {
+        return {
+            type: "sqlite",
+            database: ":memory:",
+            synchronize: true,
+            entities: [entitiesPath], 
+        };
+    }
+
+    return {
         type: "postgres",
         url: process.env.DATABASE_URL,
         ssl: process.env.NODE_END === "production" ?
@@ -21,6 +38,38 @@ const AppDataSource = new DataSource(
         entities: process.env.NODE_ENV === "production" ? ["dist/entities/*.js"] : ['src/entities/*.ts'],
         migrations: process.env.NODE_ENV === "production" ? ["dist/migrations/*.js"] : ['src/migrations/*.ts']
     }
-)
 
-export default AppDataSource
+
+
+
+
+}
+
+
+const dataSourceConfigs = setDataSourceConfig();
+export default new DataSource(dataSourceConfigs);
+
+
+
+// const AppDataSource = new DataSource(
+//     process.env.NODE_ENV === "test"?
+//     {
+//         type: "sqlite",
+//         database: ":memory:",
+//         synchronize: true,
+//         entities: ["src/entities/*.ts"]
+//     } :
+//     {
+//         type: "postgres",
+//         url: process.env.DATABASE_URL,
+//         ssl: process.env.NODE_END === "production" ?
+//         {rejectUnauthorized:false} 
+//         : false,
+
+//         logging: true,
+//         synchronize: false,
+//         entities: process.env.NODE_ENV === "production" ? ["dist/entities/*.js"] : ['src/entities/*.ts'],
+//         migrations: process.env.NODE_ENV === "production" ? ["dist/migrations/*.js"] : ['src/migrations/*.ts']
+//     }
+// )
+
